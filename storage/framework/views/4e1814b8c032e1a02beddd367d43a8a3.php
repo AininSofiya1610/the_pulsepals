a1378a7a5e7a75040ce17f764b3f52e1284e3f5c
Nur Ainin Sofiya Zainudin <aininsfyaa16@gmail.com>
2026-04-09 08:17:06 +0800

    Remove cached views and fix gitignore

--- storage/framework/views/4e1814b8c032e1a02beddd367d43a8a3.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\xampp\htdocs\laravel\the_pulsepals\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/panel.blade.php ENDPATH**/ ?>
--- storage/framework/views/9891438c73aa38eb584b39bd56d75818.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\xampp\htdocs\laravel\the_pulsepals\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
